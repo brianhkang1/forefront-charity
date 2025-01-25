472427e32449c7125efb6c9155489614f3c942c1
Brian Kang <30248266+brianhkang1@users.noreply.github.com>
2025-01-25 13:52:04 -0500

    major bump tailwind to v4

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,11 +2,17 @@
 
 const path = require('path');
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+// TODO: breaking after Tailwind major bump to v4.
+// Find fix so that it doesn't break linting app/global.css (presumably with the tailwind directives)
+//
+// const buildEslintCommand = (filenames) =>
+//   `next lint --fix --file ${filenames
+//     .map((f) => path.relative(process.cwd(), f))
+//     .join(' --file ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx,css}': ['prettier --write', buildEslintCommand],
+  '*.{js,jsx,ts,tsx,css}': [
+    'prettier --write',
+    // buildEslintCommand
+  ],
 };
