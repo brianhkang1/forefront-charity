f9b3d16b083d2b6302250d2b80530206e2a462a6
Brian Kang <30248266+brianhkang1@users.noreply.github.com>
2025-03-22 10:11:17 -0400

    complete poc for How We Got Here

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,22 @@ import Image from '@/components/Image';
 import Title from '@/components/Title';
 import { getGoogleSheetsData } from '@/lib/googleSheets';
 import findImage from '@/utils/findImage';
+import clsx from 'clsx';
 import type { Metadata } from 'next';
 
 import { getGoogleDriveImages } from '../../lib/googleDrive';
+
+const HOW_WE_GOT_HERE = [
+  { title: 2015, description: 'First Well in Pallapatla, India' },
+  { title: 2016, description: 'Launched Handwashing Initiative' },
+  { title: 2017, description: 'Identified land for our FOREFRONT School' },
+  { title: 2018, description: 'Began building FOREFRONT School' },
+  { title: 2019, description: 'Piloted a diabetes awareness workshop' },
+  { title: 2020, description: 'Officially launched FOREFRONT School' },
+  { title: 2021, description: 'Provided COVID relief with masks and food' },
+  { title: 2022, description: 'Launched FOREFRONT Fearless' },
+  { title: 2023, description: 'Launched newborn care' },
+];
 
 // Default value, but explicitly set to ensure SSG
 export const revalidate = false;
@@ -100,6 +113,49 @@ export default async function AboutPage() {
             </div>
           </div>
         </Image>
+      </section>
+
+      <section className='mt-[-65px] h-172.5 bg-teal-800 px-9 text-white'>
+        <h2 className='pt-15 text-center'>How We Got Here</h2>
+
+        <div className='relative h-139 overflow-x-auto'>
+          {/* white line */}
+          <div className='absolute top-1/2 left-0 h-[2px] w-full bg-white' />
+
+          {HOW_WE_GOT_HERE.map(({ title, description }, index) => {
+            const isEven = index % 2 === 0;
+            const leftPosition = 160 * index;
+            const topPosition = isEven ? 9 : 274;
+
+            return (
+              <div
+                key={title}
+                className={clsx(
+                  'absolute flex w-40 items-center',
+                  isEven ? 'flex-col' : 'flex-col-reverse',
+                )}
+                style={{ top: topPosition, left: leftPosition }}
+              >
+                <Image
+                  width={160}
+                  height={185}
+                  alt={`How we got here picture ${index + 1}`}
+                  src={image1?.url || ''}
+                  className='py-2'
+                />
+
+                <div className='min-h-[32px] text-center text-xs font-bold'>
+                  {description}
+                </div>
+
+                <div className='my-2 text-2xl font-bold'>{title}</div>
+
+                {/* white dot */}
+                <div className='h-2.75 w-2.75 rounded-full bg-white' />
+              </div>
+            );
+          })}
+        </div>
       </section>
 
       <section>
